01ceef78e27b9083f46a383d2c738c7931763b89
anubis <aw3852@nyu.edu>
2020-08-05 02:10:38 -0400

    Fixed Font Awesome Icon Rendering for nextjs

--- pages_/_app.tsx
+++ pages_/_app.tsx
@@ -1,9 +1,8 @@
 import App from 'next/app'
 import Providers from '../components/Providers'
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+import { config } from '@fortawesome/fontawesome-free'
+import '@fortawesome/fontawesome-free/styles.css'
+config.autoAddCss = false
 
 export default class ScreenApp extends App {
   render () {
